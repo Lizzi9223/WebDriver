1d974a39b8db1ef7889010b5e72336d62934fe64
Elizaveta Yakovleva <lizaveta9223@gmail.com>
2021-11-14 15:01:00 +0300

    xpath & timeout edit

--- src/main/java/pageobject_model/ExnessHomePage.java
+++ src/main/java/pageobject_model/ExnessHomePage.java
@@ -1,21 +1,27 @@
 package pageobject_model;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class ExnessHomePage extends AbstractPage{
 
     private final String HOME_PAGE_URL = "https://exness.com";
 
-    @FindBy(xpath = "//div[@class='sidebar-tabs__buttons']/a")
-    private WebElement signInButton;
+    private final By locatorSignInButton = By.xpath("//div[@class='sidebar-tabs__buttons']/a");
 
     public ExnessHomePage(WebDriver driver) {
         super(driver);
     }
 
     public ExnessSignInPage openLoginPage() {
+        WebElement signInButton = new WebDriverWait(driver, Duration.ofSeconds(WAIT_TIMEOUT_SECONDS))
+                .until(ExpectedConditions.presenceOfElementLocated(locatorSignInButton));
         String currentHandle = driver.getWindowHandle();
         String URLToClick = signInButton.getAttribute("href");
         driver.get(URLToClick);
